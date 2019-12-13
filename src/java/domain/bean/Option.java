500d5c1c1f7fae5f97780f040e63664234498ae6
Nuru <abdoulniyigena@gmail.com>
2019-12-13 17:43:35 +0200

    create an option model

--- src/java/domain/bean/Option.java
+++ src/java/domain/bean/Option.java
@@ -3,39 +3,50 @@ package domain.bean;
 
 import java.io.Serializable;
 
-public class Option implements Serializable{
-        private String id;
-        private String name;
-        private double price;
+public class Option implements Serializable {
+    private int id;
+    private String name;
+    private double price;
 
-        public Option(String name, double price) {
-            this.name = name;
-            this.price = price;
-        }
+    public Option() {
+    }
 
-        public String getName() {
-            return name;
-        }
+    public Option(String name) {
+        this.name = name;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public Option(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 
-        public double getPrice() {
-            return price;
-        }
+    public Option(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 
-        public String getId() {
-            return id;
-        }
+    public int getId() {
+        return id;
+    }
 
-        public void setId(String id) {
-            this.id = id;
-        }
-        
-        
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        public void setPrice(double price) {
-            this.price = price;
-        }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
