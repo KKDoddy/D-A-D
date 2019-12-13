f2b3904bba1bdbebbb64f3cb4bcb3aa8a93f746c
Nuru <abdoulniyigena@gmail.com>
2019-12-13 17:51:09 +0200

    Fix errors in pizzaconfig model

--- src/java/domain/bean/PizzaConfig.java
+++ src/java/domain/bean/PizzaConfig.java
@@ -1,56 +1,76 @@
 package domain.bean;
 
+import domain.enums.Size;
 import java.io.Serializable;
+import java.util.List;
 
-
-public class PizzaConfig implements Serializable{
-
-    private String id;
+public class PizzaConfig implements Serializable {
+    private int id;
+    private String name;
+    private List<OptionSet> choice;
     private double basePrice;
-    private OptionSet options[];
+    private double delivery;
+    private Size size;
 
-    public PizzaConfig(double basePrice,int maxOptionSets) {
+    public PizzaConfig() {}
+
+    public PizzaConfig(String name, double basePrice, Size size) {
+        this.name = name;
         this.basePrice = basePrice;
-        this.options = new OptionSet[maxOptionSets];
+        this.size = size;
     }
 
-    public double getBasePrice() {
-        return basePrice;
+    public PizzaConfig(String name, List<OptionSet> choice, double basePrice, double delivery, Size size) {
+        this.name = name;
+        this.choice = choice;
+        this.basePrice = basePrice;
+        this.delivery = delivery;
+        this.size = size;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
-    
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<OptionSet> getChoice() {
+        return choice;
+    }
+
+    public void setChoice(List<OptionSet> choice) {
+        this.choice = choice;
+    }
+
+    public double getBasePrice() { return basePrice; }
 
     public void setBasePrice(double basePrice) {
         this.basePrice = basePrice;
     }
 
-    public OptionSet[] getOptions() {
-        return options;
+    public double getDelivery() {
+        return delivery;
     }
 
-    public void setOptions(OptionSet[] options) {
-        for(int i=0; i<options.length; i++){
-            if(options[i]!=null){
-                this.options[i] = options[i];
-            }
-        }
-        setPrices();
+    public void setDelivery(double delivery) {
+        this.delivery = delivery;
     }
-    
-    public void setPrices(){
-        for(OptionSet set: options){
-            if(set != null){
-                set.setPrice(set.getPrice() + this.basePrice);
-            }
-        }
+
+    public Size getSize() {
+        return size;
     }
-    
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
 }
